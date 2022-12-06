6454be7bf0b3bcaadd0115b79e9d5a7df6cd4b5b
Mark Waltz <mwaltz@wisc.edu>
2022-12-06 16:05:02 -0600

    Adding split off variables and outputs file

--- main.tf
+++ main.tf
@@ -1,36 +1,18 @@
-# This file will create an auto scaling group with a launch configuration 
-# and an ALB to route traffic to the instances in the auto scaling group.
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-east-2"
 }
 
-# This variable will be used to set the port the web server will listen on
-variable "server_port" {
-  description = "The port the server will use for HTTP requests"
-  type        = number
-  default     = 8080
-}
-
-# This output will print the DNS name of the ALB
-output "alb_dns_name" {
-  value       = aws_lb.example.dns_name
-  description = "The domain name of the load balancer"
-}
-
-# This data source will find the default VPC
-data "aws_vpc" "default" {
-  default = true
-}
-
-# This data source will find all subnets in the default VPC
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-
-# This launch configuration will create an EC2 instance with the specified AMI and instance type.
 resource "aws_launch_configuration" "example" {
   image_id        = "ami-0fb653ca2d3203ac1"
   instance_type   = "t2.micro"
@@ -42,18 +24,16 @@ resource "aws_launch_configuration" "example" {
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
-# Required when using a launch configuration with an auto scaling group.
+  # Required when using a launch configuration with an auto scaling group.
   lifecycle {
     create_before_destroy = true
   }
 }
 
-# This auto scaling group will launch instances using the launch configuration
 resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
-  vpc_zone_identifier = data.aws_subnets.default.ids
-  
-  # This will add the ALB as a target group for the auto scaling group
+  vpc_zone_identifier  = data.aws_subnets.default.ids
+
   target_group_arns = [aws_lb_target_group.asg.arn]
   health_check_type = "ELB"
 
@@ -67,9 +47,8 @@ resource "aws_autoscaling_group" "example" {
   }
 }
 
-# This security group allows HTTP traffic on the port specified by the server_port variable
 resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
+  name = var.instance_security_group_name
 
   ingress {
     from_port   = var.server_port
@@ -79,17 +58,25 @@ resource "aws_security_group" "instance" {
   }
 }
 
-# This ALB will listen on port 80 and forward requests to the auto scaling group
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
 resource "aws_lb" "example" {
-  name               = "terraform-asg-example"
+
+  name               = var.alb_name
+
   load_balancer_type = "application"
   subnets            = data.aws_subnets.default.ids
   security_groups    = [aws_security_group.alb.id]
 }
-
-# This listener configures the ALB to listen on the default HTTP port, port 80, 
-# use HTTP as the protocol, and send a simple 404 page as the default response 
-# for requests that don’t match any listener rules.
 
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.example.arn
@@ -108,30 +95,10 @@ resource "aws_lb_listener" "http" {
   }
 }
 
-# This target group will forward requests to the auto scaling group
-resource "aws_security_group" "alb" {
-  name = "terraform-example-alb"
-
-  # Allow inbound HTTP requests
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Allow all outbound requests
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# This target group will health check the instances in the auto scaling group
 resource "aws_lb_target_group" "asg" {
-  name     = "terraform-asg-example"
+
+  name = var.alb_name
+
   port     = var.server_port
   protocol = "HTTP"
   vpc_id   = data.aws_vpc.default.id
@@ -147,7 +114,6 @@ resource "aws_lb_target_group" "asg" {
   }
 }
 
-# Ties everything together by creating a listener rule that forwards all requests to the target group using aws_lb_listener_rule.
 resource "aws_lb_listener_rule" "asg" {
   listener_arn = aws_lb_listener.http.arn
   priority     = 100
@@ -161,6 +127,27 @@ resource "aws_lb_listener_rule" "asg" {
   action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+resource "aws_security_group" "alb" {
+
+  name = var.alb_security_group_name
+
+  # Allow inbound HTTP requests
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound requests
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
